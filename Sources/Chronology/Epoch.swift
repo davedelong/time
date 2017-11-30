bd56ea9774de5627b23c1b0e0c9ec42a07e46126
Dave DeLong <davedelong@me.com>
2017-11-29 22:52:59 -0700

    Make Epoch Hashable

--- Sources/Chronology/Epoch.swift
+++ Sources/Chronology/Epoch.swift
@@ -7,11 +7,17 @@
 
 import Foundation
 
-public struct Epoch {
+public struct Epoch: Hashable {
+    
+    public static func ==(lhs: Epoch, rhs: Epoch) -> Bool {
+        return lhs.offsetFromReferenceDate == rhs.offsetFromReferenceDate
+    }
     
     public static let reference = Epoch(0)
     
     public static let unix = Epoch(-Date.timeIntervalBetween1970AndReferenceDate)
+    
+    public var hashValue: Int { return Int(offsetFromReferenceDate) }
     
     internal let offsetFromReferenceDate: TimeInterval
     
