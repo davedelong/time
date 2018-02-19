653ed1685cb6a421f1705952f5d05f1b1c81afb0
Dave DeLong <davedelong@me.com>
2018-02-19 13:54:17 -0700

    current → this
    This matches the naming around "thisInstant()"

--- Sources/Chronology/Clock+CurrentValues.swift
+++ Sources/Chronology/Clock+CurrentValues.swift
@@ -9,20 +9,20 @@ import Foundation
 
 public extension Clock {
     
-    public func currentEra() -> Era { return Era(instant: thisInstant(), region: region) }
-    public func currentYear() -> Year { return Year(instant: thisInstant(), region: region) }
-    public func currentYearMonth() -> YearMonth { return YearMonth(instant: thisInstant(), region: region) }
-    public func currentYearMonthDay() -> YearMonthDay { return YearMonthDay(instant: thisInstant(), region: region) }
-    public func currentYearMonthDayHour() -> YearMonthDayHour { return YearMonthDayHour(instant: thisInstant(), region: region) }
-    public func currentYearMonthDayHourMinute() -> YearMonthDayHourMinute { return YearMonthDayHourMinute(instant: thisInstant(), region: region) }
-    public func currentYearMonthDayHourMinuteSecond() -> YearMonthDayHourMinuteSecond { return YearMonthDayHourMinuteSecond(instant: thisInstant(), region: region) }
-    public func currentYearMonthDayHourMinuteSecondNanosecond() -> YearMonthDayHourMinuteSecondNanosecond { return YearMonthDayHourMinuteSecondNanosecond(instant: thisInstant(), region: region) }
+    public func thisEra() -> Era { return Era(instant: thisInstant(), region: region) }
+    public func thisYear() -> Year { return Year(instant: thisInstant(), region: region) }
+    public func thisYearMonth() -> YearMonth { return YearMonth(instant: thisInstant(), region: region) }
+    public func thisYearMonthDay() -> YearMonthDay { return YearMonthDay(instant: thisInstant(), region: region) }
+    public func thisYearMonthDayHour() -> YearMonthDayHour { return YearMonthDayHour(instant: thisInstant(), region: region) }
+    public func thisYearMonthDayHourMinute() -> YearMonthDayHourMinute { return YearMonthDayHourMinute(instant: thisInstant(), region: region) }
+    public func thisYearMonthDayHourMinuteSecond() -> YearMonthDayHourMinuteSecond { return YearMonthDayHourMinuteSecond(instant: thisInstant(), region: region) }
+    public func thisYearMonthDayHourMinuteSecondNanosecond() -> YearMonthDayHourMinuteSecondNanosecond { return YearMonthDayHourMinuteSecondNanosecond(instant: thisInstant(), region: region) }
     
-    public func currentMonth() -> Month { return Month(instant: thisInstant(), region: region) }
-    public func currentDay() -> Day { return Day(instant: thisInstant(), region: region) }
-    public func currentHour() -> Hour { return Hour(instant: thisInstant(), region: region) }
-    public func currentMinute() -> Minute { return Minute(instant: thisInstant(), region: region) }
-    public func currentSecond() -> Second { return Second(instant: thisInstant(), region: region) }
-    public func currentNanosecond() -> Nanosecond { return Nanosecond(instant: thisInstant(), region: region) }
+    public func thisMonth() -> Month { return Month(instant: thisInstant(), region: region) }
+    public func thisDay() -> Day { return Day(instant: thisInstant(), region: region) }
+    public func thisHour() -> Hour { return Hour(instant: thisInstant(), region: region) }
+    public func thisMinute() -> Minute { return Minute(instant: thisInstant(), region: region) }
+    public func thisSecond() -> Second { return Second(instant: thisInstant(), region: region) }
+    public func thisNanosecond() -> Nanosecond { return Nanosecond(instant: thisInstant(), region: region) }
     
 }
