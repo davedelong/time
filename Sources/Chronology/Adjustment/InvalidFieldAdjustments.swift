dce90435b02eb5f8f4936b4c5a1023fd621f5f84
Dave DeLong <davedelong@me.com>
2018-02-21 12:12:12 -0700

    Declare invalid subtractions as well.
    I wish there was an easier way to generate this sort of code rather than copying and pasting. This is getting somewhat tedious

--- Sources/Chronology/Adjustment/InvalidFieldAdjustments.swift
+++ Sources/Chronology/Adjustment/InvalidFieldAdjustments.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+/// INVALID ADDITION OPERATORS
+
 @available(*, unavailable, message: "Adding months to a year is invalid")
 public func +(lhs: Year, rhs: FieldAdjustment<Month>) -> Never { invalid() }
 @available(*, unavailable, message: "Adding days to a year is invalid")
@@ -54,3 +56,53 @@ public func +(lhs: YearMonthDayHourMinute, rhs: FieldAdjustment<Nanosecond>) -> 
 
 @available(*, unavailable, message: "Adding nanoseconds to a second is invalid")
 public func +(lhs: YearMonthDayHourMinuteSecond, rhs: FieldAdjustment<Nanosecond>) -> Never { invalid() }
+
+/// INVALID SUBTRACTION OPERATORS
+
+@available(*, unavailable, message: "Subtracting months from a year is invalid")
+public func -(lhs: Year, rhs: FieldAdjustment<Month>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting days from a year is invalid")
+public func -(lhs: Year, rhs: FieldAdjustment<Day>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting hours from a year is invalid")
+public func -(lhs: Year, rhs: FieldAdjustment<Hour>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting minutes from a year is invalid")
+public func -(lhs: Year, rhs: FieldAdjustment<Minute>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting seconds from a year is invalid")
+public func -(lhs: Year, rhs: FieldAdjustment<Second>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting nanoseconds from a year is invalid")
+public func -(lhs: Year, rhs: FieldAdjustment<Nanosecond>) -> Never { invalid() }
+
+@available(*, unavailable, message: "Subtracting days from a month is invalid")
+public func -(lhs: YearMonth, rhs: FieldAdjustment<Day>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting hours from a month is invalid")
+public func -(lhs: YearMonth, rhs: FieldAdjustment<Hour>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting minutes from a month is invalid")
+public func -(lhs: YearMonth, rhs: FieldAdjustment<Minute>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting seconds from a month is invalid")
+public func -(lhs: YearMonth, rhs: FieldAdjustment<Second>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting nanoseconds from a month is invalid")
+public func -(lhs: YearMonth, rhs: FieldAdjustment<Nanosecond>) -> Never { invalid() }
+
+@available(*, unavailable, message: "Subtracting hours from a day is invalid")
+public func -(lhs: YearMonthDay, rhs: FieldAdjustment<Hour>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting minutes from a day is invalid")
+public func -(lhs: YearMonthDay, rhs: FieldAdjustment<Minute>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting seconds from a day is invalid")
+public func -(lhs: YearMonthDay, rhs: FieldAdjustment<Second>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting nanoseconds from a day is invalid")
+public func -(lhs: YearMonthDay, rhs: FieldAdjustment<Nanosecond>) -> Never { invalid() }
+
+@available(*, unavailable, message: "Subtracting minutes from an hour is invalid")
+public func -(lhs: YearMonthDayHour, rhs: FieldAdjustment<Minute>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting seconds from an hour is invalid")
+public func -(lhs: YearMonthDayHour, rhs: FieldAdjustment<Second>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting nanoseconds from an hour is invalid")
+public func -(lhs: YearMonthDayHour, rhs: FieldAdjustment<Nanosecond>) -> Never { invalid() }
+
+@available(*, unavailable, message: "Subtracting seconds from a minute is invalid")
+public func -(lhs: YearMonthDayHourMinute, rhs: FieldAdjustment<Second>) -> Never { invalid() }
+@available(*, unavailable, message: "Subtracting nanoseconds from a minute is invalid")
+public func -(lhs: YearMonthDayHourMinute, rhs: FieldAdjustment<Nanosecond>) -> Never { invalid() }
+
+@available(*, unavailable, message: "Subtracting nanoseconds from a second is invalid")
+public func -(lhs: YearMonthDayHourMinuteSecond, rhs: FieldAdjustment<Nanosecond>) -> Never { invalid() }
