f142c0e171fd27eaebaec9318224db42e023e829
Sam Krishna <sam.krishna@gmail.com>
2018-08-19 22:16:03 -0700

    Added .xcodeproject files to keep code additions effective

---
+++ Supporting Files/Chronology.h
@@ -0,0 +1,20 @@
+//
+//  Chronology.h
+//  Chronology
+//
+//  Created by Sam Krishna on 8/19/18.
+//  Copyright © 2018 Sam Krishna. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Chronology.
+FOUNDATION_EXPORT double ChronologyVersionNumber;
+
+//! Project version string for Chronology.
+FOUNDATION_EXPORT const unsigned char ChronologyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chronology/PublicHeader.h>
+
+
+
