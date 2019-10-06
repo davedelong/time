07d7f6b57920d02ef9d68ef0641b565db069ca82
Dave DeLong <davedelong@me.com>
2019-10-06 15:25:09 -0600

    Handle autoupdating region values
    When a DateFormatter is given an autoupdating Locale value, the values from that locale will override any custom calendar set on the date formatter, preferring instead the calendar from the autoupdating locale.
    
    This change makes it so that DateFormatters are _always_ built using non-autoupdating versions of the region components. However, the DateFormatterCache will remember which formatters were created from autoupdating Regions, and then un-cache them if something about the autoupdating Region changes.

--- Sources/Chronology/Utilities/DateFormatterCache.swift
+++ Sources/Chronology/Utilities/DateFormatterCache.swift
@@ -21,28 +21,78 @@ internal class DateFormatterCache {
     }
     
     private let queue = DispatchQueue(label: "DateFormatterCache")
+    
     private var formatters = Dictionary<Key, DateFormatter>()
+    
+    private var registeredForAutoupdatingNotifications = false
+    private var autoupdating = Dictionary<Key, DateFormatter>()
     
     private init() { }
     
+    private func onqueue_registerForAutoupdatingChanges() {
+        guard registeredForAutoupdatingNotifications == false else { return }
+        
+        let observer: (Notification) -> Void = { [weak self] _ in
+            self?.resetAutoupdatingFormatters()
+        }
+        
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.NSSystemTimeZoneDidChange, object: nil, queue: nil, using: observer)
+        NotificationCenter.default.addObserver(forName: NSLocale.currentLocaleDidChangeNotification, object: nil, queue: nil, using: observer)
+        
+        registeredForAutoupdatingNotifications = true
+    }
+    
+    private func resetAutoupdatingFormatters() {
+        queue.async {
+            self.autoupdating.removeAll()
+        }
+    }
+    
+    private func onqueue_autoupdatingFormatter(for key: Key) -> DateFormatter {
+        if let existing = autoupdating[key] { return existing }
+        
+        let formatter = DateFormatter()
+        formatter.locale = key.region.locale.isAutoupdating ? .current : key.region.locale
+        formatter.calendar = key.region.calendar.isAutoupdating ? .current : key.region.calendar
+        formatter.timeZone = key.region.timeZone.isAutoupdating ? .current : key.region.timeZone
+        switch key.configuration {
+            case .template(let template):
+                formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: template, options: 0, locale: key.region.locale)
+            case .styles(let date, let time):
+                formatter.dateStyle = date
+                formatter.timeStyle = time
+        }
+        formatters[key] = formatter
+        
+        return formatter
+    }
+    
+    private func onqueue_nonupdatingFormatter(for key: Key) -> DateFormatter {
+        if let existing = formatters[key] { return existing }
+        
+        let formatter = DateFormatter()
+        formatter.locale = key.region.locale
+        formatter.calendar = key.region.calendar
+        formatter.timeZone = key.region.timeZone
+        switch key.configuration {
+            case .template(let template):
+                formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: template, options: 0, locale: key.region.locale)
+            case .styles(let date, let time):
+                formatter.dateStyle = date
+                formatter.timeStyle = time
+        }
+        formatters[key] = formatter
+        
+        return formatter
+    }
+    
     private func formatter(for key: Key) -> DateFormatter {
         return queue.sync {
-            if let existing = formatters[key] { return existing }
-            
-            let formatter = DateFormatter()
-            formatter.locale = key.region.locale
-            formatter.calendar = key.region.calendar
-            formatter.timeZone = key.region.timeZone
-            switch key.configuration {
-                case .template(let template):
-                    formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: template, options: 0, locale: key.region.locale)
-                case .styles(let date, let time):
-                    formatter.dateStyle = date
-                    formatter.timeStyle = time
+            if key.region.isAutoupdating {
+                return onqueue_autoupdatingFormatter(for: key)
+            } else {
+                return onqueue_nonupdatingFormatter(for: key)
             }
-            formatters[key] = formatter
-            
-            return formatter
         }
     }
     
