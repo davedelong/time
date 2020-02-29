19e9f4ef72bb2341a759011d70a68643784bdaf2
Dave DeLong <davedelong@me.com>
2020-02-29 09:01:31 -0800

    shuffling

--- Sources/Time/Differences/Difference.swift
+++ Sources/Time/Differences/Difference.swift
@@ -27,31 +27,83 @@ public struct Difference<Smallest: Unit, Largest: Unit> {
 }
 
 public extension Difference where Smallest: LTOEYear, Largest: GTOEYear {
+    static func eras(_ value: Int) -> Difference { return self.init(value: value, unit: .era) }
+
+    /// Retrieve the number of eras in a calendrical difference
+    var eras: Int {
+        return dateComponents.era
+            .unwrap("A Difference<\(Smallest.self), \(Largest.self)> must have an era value")
+    }
+}
+
+public extension Difference where Smallest: LTOEYear, Largest: GTOEYear {
     static func years(_ value: Int) -> Difference { return self.init(value: value, unit: .year) }
+    
+    /// Retrieve the number of years in a calendrical difference
+    var years: Int {
+        return dateComponents.year
+            .unwrap("A Difference<\(Smallest.self), \(Largest.self)> must have a year value")
+    }
 }
 
 public extension Difference where Smallest: LTOEMonth, Largest: GTOEMonth {
     static func months(_ value: Int) -> Difference { return self.init(value: value, unit: .month) }
+    
+    /// Retrieve the number of months in a calendrical difference
+    var months: Int {
+        return dateComponents.month
+            .unwrap("A Difference<\(Smallest.self), \(Largest.self)> must have a month value")
+    }
 }
 
 public extension Difference where Smallest: LTOEDay, Largest: GTOEDay {
     static func days(_ value: Int) -> Difference { return self.init(value: value, unit: .day) }
+    
+    /// Retrieve the number of days in a calendrical difference
+    var days: Int
+    {
+        return dateComponents.day.unwrap("A Difference<\(Smallest.self), \(Largest.self)> must have a day value")
+    }
 }
 
 public extension Difference where Smallest: LTOEHour, Largest: GTOEHour {
     static func hours(_ value: Int) -> Difference { return self.init(value: value, unit: .hour) }
+    
+    /// Retrieve the number of hours in a calendrical difference
+    var hours: Int {
+        return dateComponents.hour
+            .unwrap("A Difference<\(Smallest.self), \(Largest.self)> must have an hour value")
+    }
 }
 
 public extension Difference where Smallest: LTOEMinute, Largest: GTOEMinute {
     static func minutes(_ value: Int) -> Difference { return self.init(value: value, unit: .minute) }
+    
+    /// Retrieve the number of minutes in a calendrical difference
+    var minutes: Int {
+        return dateComponents.minute
+            .unwrap("A Difference<\(Smallest.self), \(Largest.self)> must have a minute value")
+    }
 }
 
 public extension Difference where Smallest: LTOESecond, Largest: GTOESecond {
     static func seconds(_ value: Int) -> Difference { return self.init(value: value, unit: .second) }
+    
+    /// Retrieve the number of seconds in a calendrical difference
+    var seconds: Int {
+        return dateComponents.second
+            .unwrap("A Difference<\(Smallest.self), \(Largest.self)> must have a second value")
+    }
 }
 
 public extension Difference where Smallest: LTOENanosecond, Largest: GTOENanosecond {
     static func nanoseconds(_ value: Int) -> Difference { return self.init(value: value, unit: .nanosecond) }
+    
+    /// Retrieve the number of nanoseconds in a calendrical difference
+    var nanoseconds: Int {
+        return dateComponents.nanosecond
+            .unwrap("A Difference<\(Smallest.self), \(Largest.self)> must have a nanosecond value")
+    }
 }
 
 // absolute adjustment
@@ -64,10 +116,10 @@ public func -<S, L>(lhs: Value<S, L>, rhs: Difference<S, L>) -> Value<S, L> wher
 }
 
 // relative adjustment
-public func +<S, L>(lhs: Value<S, L>, rhs: Difference<S, L>) -> Value<S, L> where L: LTOEYear {
+internal func +<S, L>(lhs: Value<S, L>, rhs: Difference<S, L>) -> Value<S, L> where L: LTOEYear {
     fatalError("Unimplemented")
 }
 
-public func -<S, L>(lhs: Value<S, L>, rhs: Difference<S, L>) -> Value<S, L> where L: LTOEYear {
+internal func -<S, L>(lhs: Value<S, L>, rhs: Difference<S, L>) -> Value<S, L> where L: LTOEYear {
     return lhs + rhs.negated()
 }
