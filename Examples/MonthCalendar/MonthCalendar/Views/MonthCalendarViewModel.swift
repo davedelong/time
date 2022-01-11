964559a2b8e5298ecc486345c394c25f45e7efdb
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2022-01-11 11:04:28 +0100

    Generate month days in the MonthCalendar sample app

--- Examples/MonthCalendar/MonthCalendar/Views/MonthCalendarViewModel.swift
+++ Examples/MonthCalendar/MonthCalendar/Views/MonthCalendarViewModel.swift
@@ -11,4 +11,27 @@ import Time
 
 final class MonthCalendarViewModel: ObservableObject {
     
+    // MARK: Properties
+    
+    @Published
+    private var month: Absolute<Month>
+    
+    @Published
+    private var days: AbsoluteTimePeriodSequence<Day> {
+        didSet {
+            dayTitles = days.map(\.description)
+        }
+    }
+    
+    var yearTitle: String { month.year.description }
+    var monthTitle: String { month.description }
+    var dayTitles: [String] = []
+    
+    // MARK: Initializer
+    
+    init() {
+        let month = Clocks.system.thisMonth()
+        self.month = month
+        days = month.days
+    }
 }
