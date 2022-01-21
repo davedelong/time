be073d76d6984b1fa09789a92f7332884468a2b2
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2022-01-21 10:47:46 +0100

    Only allow portrait orientation in MonthCalendar

--- Examples/MonthCalendar/MonthCalendar/Application/MonthCalendarApp.swift
+++ Examples/MonthCalendar/MonthCalendar/Application/MonthCalendarApp.swift
@@ -6,12 +6,37 @@
 //
 
 import SwiftUI
+import UIKit
+
+// MARK: - Application
 
 @main
 struct MonthCalendarApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self)
+    private var delegate
+    
     var body: some Scene {
         WindowGroup {
             MonthCalendar()
         }
+    }
+}
+
+// MARK: - Delegate
+
+fileprivate final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        true
+    }
+    
+    func application(
+        _ application: UIApplication,
+        supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
+        .portrait
     }
 }
