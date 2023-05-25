282392b91106e5b5ebca561ee993b21f7e4b3b11
Dave DeLong <davedelong@me.com>
2023-05-25 09:43:23 -0600

    more unit tests

--- Tests/TimeTests/ReportedBugs.swift
+++ Tests/TimeTests/ReportedBugs.swift
@@ -10,7 +10,7 @@ import Time
 
 class ReportedBugs: XCTestCase {
     
-    func testSwedenRangeCrash() throws {
+    func testSwedenRangeCrashAroundDST() throws {
         let sweden = Region(calendar: Calendar(identifier: .gregorian),
                             timeZone: TimeZone(identifier: "Europe/Stockholm")!,
                               locale: Locale(identifier: "en_US"))
@@ -18,6 +18,24 @@ class ReportedBugs: XCTestCase {
         let start = try Absolute<Second>(region: sweden, year: 2023, month: 10, day: 29, hour: 02, minute: 30, second: 00)
         let end = start.adding(minutes: 30)
         let range = start ..< end
+        
+        XCTAssertTrue(range.upperBound > range.lowerBound)
+    }
+    
+    func testSwedenRepeatedOffsettingAroundDST() throws {
+        let sweden = Region(calendar: Calendar(identifier: .gregorian),
+                            timeZone: TimeZone(identifier: "Europe/Stockholm")!,
+                              locale: Locale(identifier: "en_US"))
+
+        let start = try Absolute<Second>(region: sweden, year: 2023, month: 10, day: 29, hour: 02, minute: 30, second: 00)
+        let target = start.adding(hours: 10)
+        var step = start
+
+        while step.isBefore(target) {
+            step = step.adding(minutes: 30)
+        }
+        
+        XCTAssertTrue(step >= target)
     }
     
 }
