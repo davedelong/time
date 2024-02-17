185faa62b1b8d2b40981e289f983bd2d08527e6d
Dave DeLong <davedelong@me.com>
2024-02-17 16:09:01 -0700

    Rename ClockChime → ClockStrikes

--- Sources/Time/3-RegionalClock/RegionalClock+Strikes.swift
+++ Sources/Time/3-RegionalClock/RegionalClock+Strikes.swift
@@ -8,43 +8,43 @@ import Dispatch
 
 extension RegionalClock {
     
-    /// Sets up a repeating chime (ex: "every 5 minutes"), optionally starting at a time other than the present instant.
+    /// Sets up a repeating strike (ex: "every 5 minutes"), optionally starting at a time other than the present instant.
     ///
-    /// - Note: The first "chime" will not occur until *after* `interval` has passed beyond `startTime` (or
+    /// - Note: The first "strike" will not occur until *after* `interval` has passed beyond `startTime` (or
     ///   the immediate present if `startTime` is `nil`).
     ///
     /// - Parameters:
-    ///   - interval: The amount of time that should elapse before the next chime occurs.
-    ///   - startTime: The time to start counting at before the first chime occurs.
-    /// - Returns: A ``ClockChime`` which emits fixed time values at the moment of each chime.
-    public func chime<U: Unit>(producing unit: U.Type = U.self,
-                               every interval: TimeDifference<U, Era>,
-                               startingFrom startTime: Fixed<U>? = nil) -> ClockChime<U> {
-        return ClockChime(clock: self, interval: interval, startTime: startTime)
+    ///   - interval: The amount of time that should elapse before the next strike occurs.
+    ///   - startTime: The time to start counting at before the first strike occurs.
+    /// - Returns: A ``ClockStrikes`` which emits fixed time values at the moment of each strike.
+    public func strike<U: Unit>(producing unit: U.Type = U.self,
+                                every interval: TimeDifference<U, Era>,
+                                startingFrom startTime: Fixed<U>? = nil) -> ClockStrikes<U> {
+        return ClockStrikes(clock: self, interval: interval, startTime: startTime)
     }
     
-    /// Sets up a repeating chime, optionally starting at a time other than the present instant.
+    /// Sets up a repeating strike, optionally starting at a time other than the present instant.
     ///
     /// ```
-    /// // Chime every minute
-    /// clock.chime(every: Minute.self)
+    /// // Strike every minute
+    /// clock.strike(every: Minute.self)
     /// ```
     ///
     /// - Parameters:
-    ///   - every: The unit of time that should elapse before the next chime occurs.
-    ///   - startTime: The time to start counting at before the first chime occurs.
-    /// - Returns: A ``ClockChime`` which emits fixed time values at the moment of each chime.
-    public func chime<U: Unit>(every unit: U.Type,
-                               startingFrom startTime: Fixed<U>? = nil) -> ClockChime<U> {
+    ///   - every: The unit of time that should elapse before the next strike occurs.
+    ///   - startTime: The time to start counting at before the first strike occurs.
+    /// - Returns: A ``ClockStrikes`` which emits fixed time values at the moment of each strike.
+    public func strike<U: Unit>(every unit: U.Type,
+                                startingFrom startTime: Fixed<U>? = nil) -> ClockStrikes<U> {
         let interval = TimeDifference<U, Era>(value: 1, unit: U.component)
-        return ClockChime(clock: self, interval: interval, startTime: startTime)
+        return ClockStrikes(clock: self, interval: interval, startTime: startTime)
     }
     
-    /// Sets up a repeating chime for each unit that matches the given closure.
+    /// Sets up a repeating strike for each unit that matches the given closure.
     ///
     /// ```
-    /// // Chimes every hour at *:00, *:13, *:26, *:39, *:52
-    /// clock.chime(when: { (time: Fixed<Minute>) in
+    /// // Strike every hour at *:00, *:13, *:26, *:39, *:52
+    /// clock.strike(when: { (time: Fixed<Minute>) in
     ///     time.minute % 13 == 0
     /// })
     /// ```
@@ -54,50 +54,51 @@ extension RegionalClock {
     ///   whether it should be published.
     ///   - time: A prospective time value.
     ///
-    /// - Returns: A ``ClockChime`` which emits fixed time values at the moment of each chime.
-    public func chime<U: Unit>(producing unit: U.Type = U.self,
-                               when matches: @escaping (_ time: Fixed<U>) -> Bool) -> ClockChime<U> {
-        return ClockChime(clock: self, when: matches)
+    /// - Returns: A ``ClockStrikes`` which emits fixed time values at the moment of each strike.
+    public func strike<U: Unit>(producing unit: U.Type = U.self,
+                               when matches: @escaping (_ time: Fixed<U>) -> Bool) -> ClockStrikes<U> {
+        return ClockStrikes(clock: self, when: matches)
     }
     
-    /// Sets up a single chime (ex: "at 12:00 PM").
+    /// Sets up a single strike (ex: "at 12:00 PM").
     ///
     /// Useful, for example, when you want the `RegionalClock` to "tell me when it's 3:00 PM."
-    /// If the time has already passed, then the `ClockChime` completes immediately without sending a value.
+    /// If the time has already passed, then the `ClockStrikes` completes immediately without sending a value.
     ///
-    /// - Parameter time: The time at which the chime should occur.
+    /// - Parameter time: The time at which the strike should occur.
     ///
-    /// - Returns: A ``ClockChime`` which emits the current fixed time and then completes.
-    public func chime<U: Unit>(at time: Fixed<U>) -> ClockChime<U> {
-        return ClockChime(clock: self, at: time)
+    /// - Returns: A ``ClockStrikes`` which emits the current fixed time and then completes.
+    public func strike<U: Unit>(at time: Fixed<U>) -> ClockStrikes<U> {
+        return ClockStrikes(clock: self, at: time)
     }
     
 }
 
-#warning("1.0: rename Chime -> Strike?")
 #warning("1.0: conform Chime: Sequence?")
 
-/// A type representing zero or more times at which a ``RegionalClock`` will "chime".
+/// A type representing zero or more times at which a ``RegionalClock`` will "strike".
 ///
-/// ``ClockChime`` conforms to the `AsyncSequence` protocol and can be used in async for loops:
+/// ``ClockStrikes`` conforms to the `AsyncSequence` protocol and can be used in async for loops:
 ///
 /// ```
-/// for try await nextMinute in someClock.chime(every: Minute.self) {
-///    // one clock minute has passed since the previous chime
+/// for try await nextMinute in someClock.strike(every: Minute.self) {
+///    // one clock minute has passed since the previous strike
 ///    print("The time is now \(nextMinute)")
 /// }
 /// ```
 ///
-/// On platforms where the Combine framework is available, ``ClockChime`` also conforms to the `Publisher` protocol:
+/// On platforms where the Combine framework is available, ``ClockStrikes`` also conforms to the `Publisher` protocol:
 ///
 /// ```
-/// someClock.chime(every: Minute.self)
+/// someClock.strike(every: Minute.self)
 ///          .sink { nextMinute in
 ///            print("The time is now \(nextMinute")
 ///          }
 ///          .store(in: &someCancellableSet)
 /// ```
-public struct ClockChime<U: Unit & LTOEEra> {
+///
+/// - SeeAlso: https://en.wikipedia.org/wiki/Striking_clock
+public struct ClockStrikes<U: Unit & LTOEEra> {
     
     fileprivate let clock: any RegionalClock
     fileprivate let values: AnyIterator<Fixed<U>>
@@ -107,7 +108,7 @@ public struct ClockChime<U: Unit & LTOEEra> {
         self.values = AnyIterator(iterator)
     }
     
-    /// Create a chime that will emit a value after a specified calendar interval,
+    /// Create a `ClockStrikes` that will emit a value after a specified calendar interval,
     /// when that value matches the provided predicate.
     ///
     /// - Parameters:
@@ -125,7 +126,7 @@ public struct ClockChime<U: Unit & LTOEEra> {
         self.init(clock: clock, iterator: i)
     }
     
-    /// Create a chime that emits values that match a provided predicate.
+    /// Create a `ClockStrikes` that emits values that match a provided predicate.
     /// - Parameters:
     ///   - clock: The `RegionalClock` to use for producing calendar values
     ///   - matches: Only values matching this predicate will be emitted.
@@ -134,10 +135,10 @@ public struct ClockChime<U: Unit & LTOEEra> {
         self.init(clock: clock, interval: interval, startTime: nil, predicate: matches)
     }
     
-    /// Create a chime that emits at most one value at the specified time.
+    /// Create a `ClockStrikes` that emits at most one value at the specified time.
     /// - Parameters:
     ///   - clock: The `RegionalClock` to use for producing calendar values.
-    ///   - time: The time at which to emit the value. If this value is in the past, then the `ClockChime` emits no values.
+    ///   - time: The time at which to emit the value. If this value is in the past, then the `ClockStrikes` emits no values.
     public init(clock: any RegionalClock, at time: Fixed<U>) {
         let current = clock.current(U.self)
         var values = Array<Fixed<U>>()
@@ -149,7 +150,7 @@ public struct ClockChime<U: Unit & LTOEEra> {
     
 }
 
-extension ClockChime: AsyncSequence {
+extension ClockStrikes: AsyncSequence {
     
     public typealias Element = Fixed<U>
     
@@ -184,79 +185,79 @@ extension ClockChime: AsyncSequence {
 
 import Combine
 
-extension ClockChime: Combine.Publisher {
+extension ClockStrikes: Combine.Publisher {
     
     public typealias Output = Fixed<U>
     public typealias Failure = Never
     
-    /// Set up a new Combine subscription for this `ClockChime`
-    /// - Parameter subscriber: The subscriber that receives chime events
+    /// Set up a new Combine subscription for this `ClockStrikes`
+    /// - Parameter subscriber: The subscriber that receives strike events
     public func receive<S>(subscriber: S) where S: Subscriber, Failure == S.Failure, Output == S.Input {
-        let subscription = ChimeSubscription(subscriber: subscriber,
-                                             clock: clock,
-                                             iterator: values)
+        let subscription = StrikesSubscription(subscriber: subscriber,
+                                               clock: clock,
+                                               iterator: values)
         subscriber.receive(subscription: subscription)
     }
     
 }
 
-private class ChimeSubscription<SubscriberType, U>: Subscription
+private class StrikesSubscription<SubscriberType, U>: Subscription
     where U: Unit,
           SubscriberType: Subscriber,
-          SubscriberType.Failure == ClockChime<U>.Failure,
-          SubscriberType.Input == ClockChime<U>.Output {
+          SubscriberType.Failure == ClockStrikes<U>.Failure,
+          SubscriberType.Input == ClockStrikes<U>.Output {
     
     private var subscriber: SubscriberType?
     private let clock: any RegionalClock
     private var timeIterator: AnyIterator<Fixed<U>>
-    private var nextChime: DispatchWorkItem?
+    private var nextStrike: DispatchWorkItem?
     
     init(subscriber: SubscriberType, clock: any RegionalClock, iterator: AnyIterator<Fixed<U>>) {
         self.subscriber = subscriber
         self.clock = clock
         self.timeIterator = iterator
         
-        scheduleNextChime()
+        scheduleNextStrike()
     }
     
-    private func scheduleNextChime() {
+    private func scheduleNextStrike() {
         var nextTime: Fixed<U>? = timeIterator.next()
         let now = clock.current(U.self)
         while let next = nextTime, next < now {
-            // make sure we never chime anything in the past
+            // make sure we never strike anything in the past
             nextTime = timeIterator.next()
         }
         
-        guard let nextChimeTime = nextTime else {
+        guard let nextStrikeTime = nextTime else {
             subscriber?.receive(completion: .finished)
             cancel()
             return
         }
         
         let clockNow = clock.now
-        let chimeInstant = nextChimeTime.firstInstant
+        let strikeInstant = nextStrikeTime.firstInstant
         
-        let realSecondsUntilChime: TimeInterval
+        let realSecondsUntilStrike: TimeInterval
         
-        if chimeInstant <= clockNow {
-            // chime already passed
-            // chime immediately
-            realSecondsUntilChime = 0
+        if strikeInstant <= clockNow {
+            // strike already passed
+            // strike immediately
+            realSecondsUntilStrike = 0
         } else {
-            let clockSecondsUntilChime = chimeInstant - clockNow
-            realSecondsUntilChime = (clockSecondsUntilChime / clock.SISecondsPerClockSecond).timeInterval
+            let clockSecondsUntilStrike = strikeInstant - clockNow
+            realSecondsUntilStrike = (clockSecondsUntilStrike / clock.SISecondsPerClockSecond).timeInterval
         }
-        let chime = DispatchWorkItem { [weak self] in
-            self?.performChime(at: nextChimeTime)
+        let strike = DispatchWorkItem { [weak self] in
+            self?.performStrike(at: nextStrikeTime)
         }
-        nextChime = chime
-        DispatchQueue.main.asyncAfter(deadline: .now() + realSecondsUntilChime, execute: chime)
+        nextStrike = strike
+        DispatchQueue.main.asyncAfter(deadline: .now() + realSecondsUntilStrike, execute: strike)
     }
     
-    private func performChime(at time: Fixed<U>) {
-        nextChime = nil
+    private func performStrike(at time: Fixed<U>) {
+        nextStrike = nil
         _ = subscriber?.receive(time)
-        scheduleNextChime()
+        scheduleNextStrike()
     }
     
     public func request(_ demand: Subscribers.Demand) {
@@ -264,8 +265,8 @@ private class ChimeSubscription<SubscriberType, U>: Subscription
     }
     
     public func cancel() {
-        nextChime?.cancel()
-        nextChime = nil
+        nextStrike?.cancel()
+        nextStrike = nil
         subscriber = nil
     }
 }
---
+++ Tests/TimeTests/ClockStrikeTests.swift
@@ -0,0 +1,297 @@
+//
+//  ClockStrikeTests.swift
+//  TimeTests
+//
+
+import XCTest
+import Time
+
+#if canImport(Combine)
+import Combine
+
+@available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+final class ClockStrikeTests: XCTestCase {
+    
+    static var allTests = [
+        ("testPastStrike", testPastStrike),
+        ("testImmediateStrike", testImmediateStrike),
+        ("testStrikeAtSpecificValue", testStrikeAtSpecificValue),
+        ("testScaledStrikeAtSpecificValue", testScaledStrikeAtSpecificValue),
+        ("testFixedStrikeCancel", testFixedStrikeCancel),
+        ("testIntervalStrike", testIntervalStrike),
+        ("testIntervalStrikeWithPastStart", testIntervalStrikeWithPastStart),
+        ("testIntervalStrikeCancel", testIntervalStrikeCancel),
+        ("testPredicateStrike", testPredicateStrike),
+    ]
+    
+    let clock = Clocks.system
+    var cancellables = Set<AnyCancellable>()
+    
+    override func tearDown() {
+        super.tearDown()
+        cancellables = []
+    }
+    
+    func testPastStrike() {
+        let dontStrike = expectation(description: "Clock never strikes")
+        dontStrike.isInverted = true
+        
+        let completes = expectation(description: "Strike completes")
+        let lastMinute = clock.previousMinute
+        
+        clock
+            .strike(at: lastMinute) // Should fire ASAP
+            .sink(receiveCompletion: { _ in
+                completes.fulfill()
+            }, receiveValue: { _ in
+                dontStrike.fulfill()
+            })
+            .store(in: &cancellables)
+        
+        wait(for: [completes, dontStrike], timeout: 0.01, enforceOrder: true)
+    }
+    
+    func testPastStrikeAsync() async throws {
+        let lastMinute = clock.previousMinute
+        
+        var strikeCount = 0
+        for try await _ in clock.strike(at: lastMinute) {
+            XCTFail("Clock strikes should skip times in the past")
+            strikeCount += 1
+        }
+        XCTAssertEqual(strikeCount, 0)
+    }
+    
+    func testImmediateStrike() {
+        let strikesOnce = expectation(description: "Clock strikes immediately")
+        let completes = expectation(description: "Strike completes")
+        let now = clock.currentSecond
+        
+        clock
+            .strike(at: now) // Should fire immediately
+            .sink(receiveCompletion: { _ in
+                completes.fulfill()
+            }, receiveValue: { value in
+                XCTAssertEqual(now, value)
+                strikesOnce.fulfill()
+            })
+            .store(in: &cancellables)
+        
+        wait(for: [strikesOnce, completes], timeout: 0.5, enforceOrder: true)
+    }
+    
+    func testImmediateStrikeAsync() async throws {
+        let start = clock.currentSecond
+        
+        var strikeCount = 0
+        for try await time in clock.strike(at: start) {
+            let now = clock.currentSecond
+            XCTAssertEqual(start, now)
+            XCTAssertEqual(time, now)
+            strikeCount += 1
+        }
+        XCTAssertEqual(strikeCount, 1)
+    }
+    
+    func testStrikeAtSpecificValue() {
+        let strikesOnce = expectation(description: "Clock strikes shortly")
+        let completes = expectation(description: "Strike completes")
+        
+        let nextSecond = clock.nextSecond
+        clock
+            .strike(at: nextSecond)
+            .sink(receiveCompletion: { _ in
+                completes.fulfill()
+            }, receiveValue: { value in
+                XCTAssertEqual(value, nextSecond)
+                strikesOnce.fulfill()
+            })
+            .store(in: &cancellables)
+        
+        wait(for: [strikesOnce, completes], timeout: 1, enforceOrder: true)
+    }
+    
+    func testStrikeAtSpecificValueAsync() async throws {
+        let nextSecond = clock.nextSecond
+        var strikeCount = 0
+        
+        for try await time in clock.strike(at: nextSecond) {
+            XCTAssertEqual(time, nextSecond)
+            strikeCount += 1
+        }
+        XCTAssertEqual(strikeCount, 1)
+    }
+    
+    func testScaledStrikeAtSpecificValue() {
+        let strikesOnce = expectation(description: "Clock strikes shortly")
+        let completes = expectation(description: "Strike completes")
+        
+        let sixtyXClock = Clocks.custom(startingFrom: clock.now, rate: 60.0, region: clock.region)
+        let nextMinute = sixtyXClock.nextMinute
+        
+        sixtyXClock
+            .strike(at: nextMinute)
+            .sink(receiveCompletion: { _ in
+                completes.fulfill()
+            }, receiveValue: { value in
+                XCTAssertEqual(value, nextMinute)
+                strikesOnce.fulfill()
+            })
+            .store(in: &cancellables)
+        
+        wait(for: [strikesOnce, completes], timeout: 1, enforceOrder: true)
+    }
+    
+    func testScaledStrikeAtSpecificValueAsync() async throws {
+        let sixtyXClock = Clocks.custom(startingFrom: clock.now, rate: 60.0, region: clock.region)
+        let nextMinute = sixtyXClock.nextMinute
+        
+        var strikeCount = 0
+        for try await time in sixtyXClock.strike(at: nextMinute) {
+            XCTAssertEqual(time, nextMinute)
+            strikeCount += 1
+        }
+        XCTAssertEqual(strikeCount, 1)
+    }
+    
+    func testFixedStrikeCancel() {
+        let nextSecond = clock.nextSecond
+        let dontStrike = expectation(description: "Clock does not strike")
+        dontStrike.isInverted = true
+        let strike = clock.strike(at: nextSecond).sink { value in
+            dontStrike.fulfill()
+        }
+        strike.cancel()
+        wait(for: [dontStrike], timeout: 1.0)
+    }
+    
+    // There's no way to make a `testFixedStrikeCancelAsync()` test, because callers cannot cancel an async for loop
+    
+    func testIntervalStrike() {
+        let strikesTwice = expectation(description: "Clock strikes twice, once per second")
+        strikesTwice.expectedFulfillmentCount = 2
+        
+        let start = clock.nextSecond
+        var results = [start, start.nextSecond]
+        print("Expecting strikes at \(results)")
+        
+        clock
+            .strike(every: TimeDifference<Second, Era>.seconds(1), startingFrom: start)
+            .sink(receiveCompletion: { (completion) in
+                XCTFail("Repeating strike completed: \(completion)")
+            }, receiveValue: { value in
+                let expected = results.removeFirst()
+                XCTAssertEqual(value, expected)
+                strikesTwice.fulfill()
+            })
+            .store(in: &cancellables)
+        
+        wait(for: [strikesTwice], timeout: 2.0)
+    }
+    
+    func testIntervalStrikeAsync() async throws {
+        let start = clock.nextSecond
+        
+        var expected = [start, start.nextSecond]
+        var strikeCount = 0
+        
+        for try await time in clock.strike(every: .seconds(1), startingFrom: start) {
+            XCTAssertEqual(time, expected.removeFirst())
+            strikeCount += 1
+            if expected.isEmpty { break }
+        }
+        
+        XCTAssertEqual(strikeCount, 2)
+    }
+    
+    func testIntervalStrikeWithPastStart() {
+        let strikes = expectation(description: "Clock strikes once")
+        
+        let thisSecond = clock.currentSecond
+        let aMinuteAgo = thisSecond.subtracting(minutes: 1)
+        
+        clock
+            .strike(every: .seconds(1), startingFrom: aMinuteAgo)
+            .sink(receiveCompletion: { (completion) in
+                XCTFail("Repeating strike completed: \(completion)")
+            }, receiveValue: { value in
+                XCTAssertEqual(value, thisSecond)
+                strikes.fulfill()
+            })
+            .store(in: &cancellables)
+        
+        wait(for: [strikes], timeout: 1)
+    }
+    
+    func testIntervalStrikeWithPastStartAsync() async throws {
+        let thisSecond = clock.currentSecond
+        let aMinuteAgo = thisSecond.subtracting(minutes: 1)
+        
+        var strikeCount = 0
+        for try await time in clock.strike(every: .seconds(1), startingFrom: aMinuteAgo) {
+            XCTAssertEqual(time, thisSecond)
+            strikeCount += 1
+            break
+        }
+        XCTAssertEqual(strikeCount, 1)
+    }
+    
+    func testIntervalStrikeCancel() {
+        let blueMoon = TimeDifference<Nanosecond, Era>.nanoseconds(1_000_000_000 / 12)
+        let bellOfStJohn = expectation(description: "The bell rings twelve times")
+        bellOfStJohn.expectedFulfillmentCount = 12
+        clock.strike(every: blueMoon).sink { _ in
+            bellOfStJohn.fulfill()
+        }.store(in: &cancellables)
+        
+        wait(for: [bellOfStJohn], timeout: 2)
+    }
+    
+    func testPredicateStrike() {
+        // Set to the next hour
+        let justBeforeNextMinute = clock.nextMinute.firstSecond.previous
+        
+        // every second is a minute
+        let fastClock = Clocks.custom(startingFrom: justBeforeNextMinute.firstInstant, rate: 60.0, region: clock.region)
+        var results = [0, 13, 26, 39, 52]
+        
+        let strikes = expectation(description: "Clock strikes 5 times in an hour")
+        strikes.expectedFulfillmentCount = 5
+        
+        fastClock
+            .strike(when: { (value: Fixed<Second>) -> Bool in
+                return value.second % 13 == 0
+            })
+            .sink(receiveValue: { value in
+                let expected = results.removeFirst()
+                XCTAssertEqual(value.second, expected)
+                strikes.fulfill()
+            })
+            .store(in: &cancellables)
+        
+        wait(for: [strikes], timeout: 10.0)
+    }
+    
+    func testPredicateStrikeAsync() async throws {
+        let justBeforeTheNextMinute = clock.nextMinute.firstSecond.previous
+        
+        let fastClock = Clocks.custom(startingFrom: justBeforeTheNextMinute.firstInstant, rate: 60.0, region: clock.region)
+        var results = [0, 13, 26, 39, 52]
+        
+        for try await time in fastClock.strike(producing: Second.self, when: { $0.second.isMultiple(of: 13) }) {
+            XCTAssertEqual(time.second, results.removeFirst())
+            if results.isEmpty { break }
+        }
+        
+        XCTAssertTrue(results.isEmpty)
+    }
+    
+}
+
+#else
+
+final class ClockStrikeTests: XCTestCase {
+    static var allTests: [(String, (ClockStrikeTests) -> () throws -> ())] = []
+}
+
+#endif
