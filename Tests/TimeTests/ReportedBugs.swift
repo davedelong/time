24d5a39a658fe78cd38ee5ea2c462351bdf37209
Dave DeLong <davedelong@me.com>
2024-03-03 10:05:42 -0700

    Make sure weekdays are consistent
    Fixes #75

--- Tests/TimeTests/ReportedBugs.swift
+++ Tests/TimeTests/ReportedBugs.swift
@@ -31,4 +31,37 @@ class ReportedBugs: XCTestCase {
         XCTAssertTrue(step >= target)
     }
     
+    func testISO8601WeekdayNumber_GH75() throws {
+        /*
+         The ISO8601 calendar uses different weekday numberings than the
+         gregorian calendar. On the gregorian calendar, 1 = Sunday. On the
+         ISO8601 calendar, 1 = Monday.
+         
+         This test makes sure that the numeric value from `.dayOfWeek` and
+         the Locale.Weekday value from `.weekday` is consistent between
+         calendars, even if they *format* differently.
+         */
+        
+        let iso8601 = Calendar(identifier: .iso8601)
+        let iso8601Region = Region(calendar: iso8601,
+                                   timeZone: TimeZone(secondsFromGMT: 0)!,
+                                   locale: .current)
+        
+        let mar3Gregorian = try Fixed(region: .posix, year: 2024, month: 3, day: 3)
+        let mar3ISO8601 = try Fixed(region: iso8601Region, year: 2024, month: 3, day: 3)
+        
+        XCTAssertEqual(mar3Gregorian.dayOfWeek, 1)
+        XCTAssertEqual(mar3ISO8601.dayOfWeek, 1)
+        
+        #if !os(Linux)
+        XCTAssertEqual(mar3Gregorian.weekday, .sunday)
+        XCTAssertEqual(mar3ISO8601.weekday, .sunday)
+        #endif
+        
+        let formattedGregorian = mar3Gregorian.format(weekday: .naturalDigits)
+        let formattedISO8601 = mar3ISO8601.format(weekday: .naturalDigits)
+        
+        XCTAssertEqual(formattedGregorian, "1")
+        XCTAssertEqual(formattedISO8601, "7")
+    }
 }
