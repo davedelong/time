afdbfcb4a18332f9aab14c4c4d60a524863a66f1
Dave DeLong <davedelong@me.com>
2024-04-06 14:13:01 -0600

    Add some invalid week adjustments

--- Sources/Time/5-Differences/TimeDifference+Invalid.swift
+++ Sources/Time/5-Differences/TimeDifference+Invalid.swift
@@ -6,6 +6,8 @@ extension Fixed where Granularity == Year {
     
     @available(*, unavailable, message: "Adding months to a year is invalid")
     public static func +<L>(lhs: Self, rhs: TimeDifference<Month, L>) -> Never { invalid() }
+    @available(*, unavailable, message: "Adding weeks to a year is invalid")
+    public static func +<L>(lhs: Self, rhs: TimeDifference<Week, L>) -> Never { invalid() }
     @available(*, unavailable, message: "Adding days to a year is invalid")
     public static func +<L>(lhs: Self, rhs: TimeDifference<Day, L>) -> Never { invalid() }
     @available(*, unavailable, message: "Adding hours to a year is invalid")
@@ -19,6 +21,8 @@ extension Fixed where Granularity == Year {
     
     @available(*, unavailable, message: "Subtracting months from a year is invalid")
     public static func -<L>(lhs: Self, rhs: TimeDifference<Month, L>) -> Never { invalid() }
+    @available(*, unavailable, message: "Subtracting weeks from a year is invalid")
+    public static func -<L>(lhs: Self, rhs: TimeDifference<Week, L>) -> Never { invalid() }
     @available(*, unavailable, message: "Subtracting days from a year is invalid")
     public static func -<L>(lhs: Self, rhs: TimeDifference<Day, L>) -> Never { invalid() }
     @available(*, unavailable, message: "Subtracting hours from a year is invalid")
@@ -33,6 +37,8 @@ extension Fixed where Granularity == Year {
 
 extension Fixed where Granularity == Month {
     
+    @available(*, unavailable, message: "Adding weeks to a month is invalid")
+    public static func +<L>(lhs: Self, rhs: TimeDifference<Week, L>) -> Never { invalid() }
     @available(*, unavailable, message: "Adding days to a month is invalid")
     public static func +<L>(lhs: Self, rhs: TimeDifference<Day, L>) -> Never { invalid() }
     @available(*, unavailable, message: "Adding hours to a month is invalid")
@@ -44,6 +50,8 @@ extension Fixed where Granularity == Month {
     @available(*, unavailable, message: "Adding nanoseconds to a month is invalid")
     public static func +<L>(lhs: Self, rhs: TimeDifference<Nanosecond, L>) -> Never { invalid() }
     
+    @available(*, unavailable, message: "Subtracting weeks from a month is invalid")
+    public static func -<L>(lhs: Self, rhs: TimeDifference<Week, L>) -> Never { invalid() }
     @available(*, unavailable, message: "Subtracting days from a month is invalid")
     public static func -<L>(lhs: Self, rhs: TimeDifference<Day, L>) -> Never { invalid() }
     @available(*, unavailable, message: "Subtracting hours from a month is invalid")
