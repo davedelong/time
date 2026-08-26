7b814ac74489100a52fe0d7a50f9468c9be3bdd2
Dave DeLong <davedelong@me.com>
2026-08-26 10:24:46 -0600

    fix timezone forcecopy issue

--- Tests/TimeTests/RegionTests.swift
+++ Tests/TimeTests/RegionTests.swift
@@ -5,6 +5,8 @@ class RegionTests: XCTestCase {
 
     static var allTests = [
         ("test24HourPreference", test24HourPreference),
+        ("testCannotCreateAutoupdatingRegion", testCannotCreateAutoupdatingRegion),
+        ("testTimezoneForcedCopy", testTimezoneForcedCopy)
     ]
     
     func test24HourPreference() {
@@ -32,6 +34,16 @@ class RegionTests: XCTestCase {
         XCTAssertFalse(autoAttempt.calendar.isLikelyAutoupdating)
         XCTAssertFalse(autoAttempt.timeZone.isLikelyAutoupdating)
         XCTAssertFalse(autoAttempt.locale.isLikelyAutoupdating)
+    }
+    
+    func testTimezoneForcedCopy() throws {
+        let etcGMTm6 = Region(calendar: Calendar(identifier: .gregorian),
+                              timeZone: TimeZone(identifier: "Etc/GMT-6")!,
+                              locale: Locale(identifier: "en_US"))
+        
+        let copy = etcGMTm6._forcedCopy()
+        
+        XCTAssertEqual(etcGMTm6.timeZone, copy.timeZone)
     }
 
 }
